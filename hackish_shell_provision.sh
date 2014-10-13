e5d1ec4a48278a8d03aaa775264a278539840810
Riku <riku@zsystem.fi>
2014-10-14 00:36:20 +0300

    Added mysql conf file for user, so that user can use mysql command without password and user.

--- hackish_shell_provision.sh
+++ hackish_shell_provision.sh
@@ -36,6 +36,15 @@ echo "$header Installing mysql..."
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
 apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+# Create mysql conf file for user
+touch /home/vagrant/.my.cnf
+cat > /home/vagrant/.my.cnf <<EOL
+[mysql]
+user = root
+password = ${MYSQL_ROOT_PASSWORD}
+pager = less
+show-warnings
+EOL
 
 
 echo ""
