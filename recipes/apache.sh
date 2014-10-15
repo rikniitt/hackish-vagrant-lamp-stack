1e8e623063dde9c37e40f2f81ce65db9267eab23
Riku <riku@zsystem.fi>
2014-10-15 16:48:04 +0300

    Enable apache rewrite module by default.

--- recipes/apache.sh
+++ recipes/apache.sh
@@ -3,3 +3,6 @@
 apt-get -y install apache2
 # Get rid of "Could not reliably determine..."
 echo ServerName $HOSTNAME >> /etc/apache2/apache2.conf
+# enable mod_rewrite
+a2enmod rewrite
+service apache2 restart
